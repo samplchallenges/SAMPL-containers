f9e6c6a307eb6829c9d1fd63217d5475e4061846
Meg Osato <megosato@Admins-MacBook-Pro.local>
2021-08-23 18:25:05 -0700

    adds base containers

---
+++ docking/examples/oedock-base/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='OE-Docking-Base',
+    version='0.1',
+    py_modules=[
+    ],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+    ''',
+)
