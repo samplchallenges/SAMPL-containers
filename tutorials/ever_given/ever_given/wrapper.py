47e710fe2d6591a194b8bb2d978e1f873d8e9a82
Meg Osato <megosato@Admins-MacBook-Pro.local>
2021-09-07 17:35:59 -0700

    updates ever_given

--- tutorials/ever_given/ever_given/wrapper.py
+++ tutorials/ever_given/ever_given/wrapper.py
@@ -1,9 +1,11 @@
 import copy
-from pathlib import Path
+import io
 import shlex
+import sys
+import threading
+from pathlib import Path
 
 import docker
-
 
 GUEST_INPUT_DIR = Path("/mnt") / "inputs"
 GUEST_OUTPUT_DIR = Path("/mnt") / "outputs"
@@ -14,8 +16,12 @@ def _guest_input_path(filename):
 
 
 def _prepare_commandline(command, args_dict):
-    return command + " " + " ".join(
-        [f"--{key} {shlex.quote(str(value))}" for key, value in args_dict.items()]
+    return (
+        command
+        + " "
+        + " ".join(
+            [f"--{key} {shlex.quote(str(value))}" for key, value in args_dict.items()]
+        )
     )
 
 
@@ -62,7 +68,35 @@ def _convert_file_kwargs(file_kwargs):
     return dirpaths, final_file_kwargs
 
 
-def run(container_uri, command="", *, file_kwargs, kwargs, output_dir=None, output_file_keys=None):
+class PrintLogHandler:
+    def handle_stdout(self, log):
+        print("stdout", log)
+
+    def handle_stderr(self, log):
+        print("stderr", log)
+
+
+def _read_stdout(container, log_handler, output_buffer):
+    for log in container.logs(stdout=True, stderr=False, stream=True):
+        log_handler.handle_stdout(log)
+        output_buffer.write(log)
+
+
+def _read_stderr(container, log_handler):
+    for log in container.logs(stdout=False, stderr=True, stream=True):
+        log_handler.handle_stderr(log)
+
+
+def run(
+    container_uri,
+    command="",
+    *,
+    file_kwargs,
+    kwargs,
+    output_dir=None,
+    output_file_keys=None,
+    log_handler=None,
+):
     """
     kwargs will be passed to container as --key=value
     where value will be shell escaped
@@ -74,39 +108,65 @@ def run(container_uri, command="", *, file_kwargs, kwargs, output_dir=None, outp
     command is optional
     Iterate over key/values of results.
     """
+    if log_handler is None:
+        log_handler = PrintLogHandler()
+
+    if not (
+        hasattr(log_handler, "handle_stdout") and hasattr(log_handler, "handle_stderr")
+    ):
+        raise TypeError("log_handler must have handle_stdout and handle_stderr methods")
+
     input_dir_map, final_file_kwargs = _convert_file_kwargs(file_kwargs)
     final_kwargs = copy.deepcopy(kwargs)
     final_kwargs.update(final_file_kwargs)
 
     final_command = _prepare_commandline(command, final_kwargs)
 
-    result = run_container(
+    running_container = run_container(
         container_uri, final_command, input_dir_map, output_dir=output_dir
     )
+
+    output_buffer = io.BytesIO()
+    out_thread = threading.Thread(
+        target=_read_stdout,
+        name="stdout",
+        args=(running_container, log_handler, output_buffer),
+    )
+    err_thread = threading.Thread(
+        target=_read_stderr, name="stderr", args=(running_container, log_handler)
+    )
+    err_thread.start()
+    out_thread.start()
+
+    # TODO: timeouts here?
+    err_thread.join()
+    out_thread.join()
+    result = output_buffer.getvalue()
     yield from _parse_output(output_dir, result, output_file_keys).items()
 
+    running_container.reload()
+    if running_container.status != "exited":
+        print(f"Container status is {running_container.status}", file=sys.stderr)
+    running_container.remove()
 
 
 def run_container(container_uri, command, inputdir_map=None, output_dir=None):
     client = docker.from_env()
     volumes = {}
     for inputdir, guest_input_dir in inputdir_map.items():
-        volumes[str(inputdir)] = {
-            'bind': str(guest_input_dir),
-            'mode': 'ro'}
+        volumes[str(inputdir)] = {"bind": str(guest_input_dir), "mode": "ro"}
     if output_dir:
         output_dir = Path(output_dir).resolve()
-        volumes[str(output_dir)] = {
-            'bind': str(GUEST_OUTPUT_DIR),
-            'mode': 'rw'}
+        volumes[str(output_dir)] = {"bind": str(GUEST_OUTPUT_DIR), "mode": "rw"}
         command = f" --output-dir {GUEST_OUTPUT_DIR} {command}"
 
-    result = client.containers.run(
+    running_container = client.containers.run(
         container_uri,
         command,
         volumes=volumes,
         network_disabled=True,
         network_mode="none",
-        remove=True,
+        remove=False,
+        detach=True,
     )
-    return result
+    return running_container
--- tutorials/ever_given/run.py
+++ tutorials/ever_given/run.py
@@ -2,6 +2,7 @@ import argparse
 from pathlib import Path
 
 from ever_given import wrapper
+
 """ --file-key will get passed into file_kwargs as --key= in file_kwargs
 into evergiven; container will get --key=filename
 
@@ -11,17 +12,22 @@ python ../ever_given/run.py robbason/calc-coords:latest --file-molfile tests/dat
 (assuming you are in app directory and you have built the container in  example_container/coords as robbason/calc-coords using its build.sh)
 """
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("container_uri")
     parser.add_argument("--command", default="")
-    parser.add_argument("--output-keys", help="comma separated list of output keys to be loaded from container output", default="")
+    parser.add_argument(
+        "--output-keys",
+        help="comma separated list of output keys to be loaded from container output",
+        default="",
+    )
     _parsed, unknown = parser.parse_known_args()
-    #print("unknown", unknown)
+    # print("unknown", unknown)
     if len(unknown) % 2 != 0:
         raise ValueError("Must pass key/value pairs")
-    for idx in range(len(unknown)//2):
-        arg = unknown[idx*2]
+    for idx in range(len(unknown) // 2):
+        arg = unknown[idx * 2]
         parser.add_argument(arg)
     args = parser.parse_args()
     kwargs = {}
@@ -29,7 +35,7 @@ def main():
     FILE_PREFIX = "file_"
     for key, value in vars(args).items():
         if key.startswith(FILE_PREFIX):
-            file_kwargs[key[len(FILE_PREFIX):]] = value
+            file_kwargs[key[len(FILE_PREFIX) :]] = value
         else:
             if key not in ("command", "container_uri", "output_keys"):
                 kwargs[key] = value
@@ -40,10 +46,16 @@ def main():
     output_dir = "evergiven_output"
     print("Putting output into", output_dir)
     results = {}
-    for k, v in wrapper.run(args.container_uri, command=args.command, file_kwargs=file_kwargs, kwargs=kwargs, output_dir=str(output_dir), output_file_keys=output_file_keys):
+    for k, v in wrapper.run(
+        args.container_uri,
+        command=args.command,
+        file_kwargs=file_kwargs,
+        kwargs=kwargs,
+        output_dir=str(output_dir),
+        output_file_keys=output_file_keys,
+    ):
         results[k] = v
     print("Results:", results)
-
 
 
 if __name__ == "__main__":
