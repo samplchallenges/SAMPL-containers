18ec9ee0d8e9cdbdbdd2c68ed190a57c8991f03c
megosato <osatom@uci.edu>
2022-05-12 17:53:52 -0700

    Update setup.py

--- docking/examples/adv-tutorial/setup.py
+++ docking/examples/adv-tutorial/setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='AutoDock-rdkit',
+    name='AutoDock-docker',
     version='0.1',
     py_modules=[
         'autodock',
         'main',
     ],
     install_requires=[
-        'Click',
     ],
     entry_points='''
         [console_scripts]
